a64a13b472993a66264a239a5fa3cc458c89df98
Antoine Pommier <140323098+AntoinePommier@users.noreply.github.com>
2024-12-06 19:02:01 +0100

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+  let newString = "";
 
+  if (n < 0) {
+    return "ERROR";
+  }
+
+  while (n > 0) {
+    newString = newString + string;
+    n--;
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
