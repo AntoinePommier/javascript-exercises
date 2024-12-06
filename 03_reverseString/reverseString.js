c26848e363eb85bf324d79c1ead84e40b7698ad5
Antoine Pommier <140323098+AntoinePommier@users.noreply.github.com>
2024-12-06 22:02:42 +0100

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let length = string.length - 1
+  let newString = ""
 
+  while (length >= 0) {
+  newString += string[length];
+    length--;
+  }
+
+  return newString;
 };
 
 // Do not edit below this line
