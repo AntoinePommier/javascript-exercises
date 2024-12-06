011e9781ad49783e81e6e2b0c742bce8c11288f7
Antoine Pommier <140323098+AntoinePommier@users.noreply.github.com>
2024-12-06 22:55:03 +0100

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let sum = 0;
+  let first = a;
+  let second = b;
 
+  if (a > b) {
+    first = b;
+    second = a;
+  }
+
+  if (a < 0 || b < 0 || typeof(a) != "number" || typeof(b) != "number" ||
+!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+
+  while (first <= second) {
+    sum += first;
+    first++;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
