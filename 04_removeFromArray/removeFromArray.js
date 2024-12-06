f3359c8c10ee029fc8a5792221a9e09237bad553
Antoine Pommier <140323098+AntoinePommier@users.noreply.github.com>
2024-12-06 22:26:30 +0100

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+  let array = args[0]
+
+   for (let i = 1 ; args[i]; i++) {
+    for (let j = 0; array[j]; j++) {
+      if (args[i] === array[j]){
+        array.splice(j, 1);
+        j--;
+      }
+    }
+   }
+
+   return array;
 };
 
 // Do not edit below this line
