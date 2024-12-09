fd513a5e10f9863ef76c3d9c0519137e69b493fe
Antoine Pommier <140323098+AntoinePommier@users.noreply.github.com>
2024-12-09 19:04:36 +0100

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = a;
+
+  while (b > 1) {
+    result *= a;
+    b--;
+  }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n == 0) {
+    return 1;
+  }
+  return factorial(n - 1) * n;
 };
 
 // Do not edit below this line
